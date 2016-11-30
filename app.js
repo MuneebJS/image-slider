d19d88bab97c9b4157fa7741ae50f1c79e6cbc57
Muneeb Khan <munibkhan7737@gmail.com>
2016-11-30 12:19:06 +0500

    slider is correctly working but there is no css or styling

---
+++ app.js
@@ -0,0 +1,46 @@
+// var getSpan = document.getElementById("span");
+// for(var i = 0; i < getSpan.length; i++) {
+//     getSpan[i].onclick = function () {
+//     }
+// }
+function change(source,spn1,spn2) {
+   img = document.getElementById("img").src = source;
+   document.getElementById(spn1).className = "hide";
+   document.getElementById(spn2).className = "show";
+}
+
+
+
+
+// for (var i = 0; i < 3; i++) {
+//     var imgs = "";
+//     getSpan.onclick = function () {
+//         imgs += "images" + i + ".jpg";
+//         document.getElementById("img").src = imgs;
+//     }
+// }
+    // getSpan.onclick =  function(){
+        // 
+            // for(var i = 0;i < 4;i++;) {
+        // imgs = "images"+i+".jpg";
+        // document.getElementById("img").src = imgs;
+        // 
+        // }
+    // }
+
+
+// function changeImg() {
+// for(var i=0; i < 4; i++) {
+// imgs = "images"+i+".jpg";
+// }
+// document.getElementById("img").src = imgs;
+// }
+
+
+// var i;
+// for (i = 0; i < 4; i++) {
+//     getSpan.addEventListener("click", function () {
+//         document.getElementsByTagName("img")[i].style.display = "none";
+//         document.getElementsByTagName("img")[1].style.display = "block";
+//     })
+// }
